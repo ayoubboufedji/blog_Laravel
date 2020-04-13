d0fec292ba3f81d6cd8d113ab951ca449b1adf2d
benazouaou <sihem.benaz@gmail.com>
2020-04-13 16:42:03 +0200

    ninth commit password reset

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+
+
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +23,37 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+/**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+    }
+
+
+
+   /* public function postforgot(Request $request) {
+        $this->validate($request,[
+            'email'=>'required|email|exists:users',
+         ]);
+
+         $user=User::whereEmail($request->email)->first();
+         //dd($user);
+
+         if($user->active ===false){
+
+            return redirect()->back()->with('error', 'Email.............');
+         }else{
+                $user->update([
+                            'activation_token'=>rand(100000,999999),
+                ]);
+          // dd($user);
+            return redirect('verifytoken')->with('success','..........email...........');
+         }
+
+    }*/
 }
